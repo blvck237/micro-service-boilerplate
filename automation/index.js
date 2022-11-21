b19cc5f4388fbf9d26d0c2d9581410bb14311177
Jordan Fokoua <jfokoua@gmail.com>
2022-11-22 00:53:48 +0100

    Remove chalk from automation script

--- automation/index.js
+++ automation/index.js
@@ -1,9 +1,7 @@
 /* eslint-disable */
 const fs = require('fs');
-const chalk = require('chalk');
 const path = require('path');
 const generatePackages = require('./generators/package.generator');
-
 
 async function serviceGeneration(data, resolve, reject) {
   const { serviceName } = data;
@@ -11,14 +9,14 @@ async function serviceGeneration(data, resolve, reject) {
   // wait for folder creation to finish then launch package generation
   return fs.mkdir(serviceFolderPath, async (err) => {
     if (err) {
-      console.log(chalk.red(error));
+      console.log(error);
       reject(err);
     } else {
       try {
         await Promise.all([generatePackages(serviceFolderPath, data, resolve, reject)]);
         resolve();
       } catch (error) {
-        console.log(chalk.red(error));
+        console.log(error);
         return reject(error);
       }
     }
